9ac44d8458aa61eafb7a421b1adfcee71332fca6
synergiecontrols <166513732+synergiecontrols@users.noreply.github.com>
2024-10-16 10:45:33 +0530

    Update settings.py

--- bom_project/bom_project/settings.py
+++ bom_project/bom_project/settings.py
@@ -37,7 +37,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME', 'bomsoftware'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'synergie'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'synergie1234'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
